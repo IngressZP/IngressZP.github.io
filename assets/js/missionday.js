cc395c3344aef5c70c9ff8ad626462a59c8ad427
Kristobal <junta.kristobal@gmail.com>
2017-11-25 17:07:07 +0200

    added lang chooser element and js

--- assets/js/missionday.js
+++ assets/js/missionday.js
@@ -7,7 +7,36 @@
 
 $(document).ready(function() {
 
-// Mission List Script
+    function setCookie(cname, cvalue, exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        var expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+
+    function getCookie(cname) {
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for(var i = 0; i <ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+
+    $('.lang-dropdown a').on('click', function (e) {
+        e.preventDefault();
+        setCookie('lang', $(this).data('lang'));
+        window.location.reload();
+    });
+
+    // Mission List Script
 
 	$('#mName li').on('mouseenter mouseleave', function (e) {
         var curClass = $(this).attr("class"); //take class
